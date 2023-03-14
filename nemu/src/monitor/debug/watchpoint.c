02f81f277cf8c51236f45dd65752ddf93d9ae5df
2013458-liu Siyang <liusiyangliu@126.com>
2023-03-14 07:53:33 -0700

    start wp

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -20,4 +20,19 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
-
+WP* new_wp() {
+	if (free_ == NULL) return NULL;
+	WP *f_top, *h_tail;
+	f_top = free_; // get one free node
+	h_tail = head; // h_tail is the first in used nodes
+	free_ = free_ -> next;
+	f_top -> next = NULL;
+	if (h_tail == NULL) head = f_top;
+	else {
+		while (h_tail -> next != NULL){
+			h_tail = h_tail -> next;
+		}
+		h_tail -> next = f_top; // link f_top on used nodes linklist as tail
+	}
+	return f_top;
+}
