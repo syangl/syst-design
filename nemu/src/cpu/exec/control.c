600110e244c67ff1792df90bdfd846f6f6f56bed
2013458-liu Siyang <liusiyangliu@126.com>
2023-04-06 07:31:42 -0700

    control

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -25,19 +25,27 @@ make_EHelper(jmp_rm) {
 
 make_EHelper(call) {
   // the target address is calculated at the decode stage
-  TODO();
+  // TODO();
+  rtl_push(&decoding.seq_eip);
+  decoding.is_jmp = 1;
 
   print_asm("call %x", decoding.jmp_eip);
 }
 
 make_EHelper(ret) {
-  TODO();
+  // TODO();
+  rtl_pop(&decoding.jmp_eip);
+  decoding.is_jmp = 1;
 
   print_asm("ret");
 }
 
 make_EHelper(call_rm) {
-  TODO();
+  // TODO();
+  t0 = decoding.seq_eip;
+  rtl_push(&t0);
+  decoding.jmp_eip = id_dest->val;
+  decoding.is_jmp = 1;
 
   print_asm("call *%s", id_dest->str);
 }
