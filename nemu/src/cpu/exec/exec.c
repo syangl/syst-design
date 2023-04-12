2f33976deb9e3bc95aea5381c9d91277571523ed
tracer-ics2017 <tracer@njuics.org>
2023-04-12 04:34:00 -0700

    >  compile 2013458 pa Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  04:34:00 up 36 min,  1 user,  load average: 1.21, 1.11, 1.00 33e94a7f41f6cf3dab621bf47b40935a458a5230

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -44,7 +44,7 @@ static make_EHelper(name) { \
 // are not debug and add into opcode_table
 make_group(gp1,
   EX(add), EMPTY, EX(adc), EX(sbb),
-  EMPTY, EX(sub), EX(xor), EX(cmp))
+  EX(and), EX(sub), EX(xor), EX(cmp))
 
   /* 0xc0, 0xc1, 0xd0, 0xd1, 0xd2, 0xd3 */
   // TODO: logic
