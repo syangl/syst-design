1d77257dce1eabf6d89b1da82976e7c88181d46d
tracer-ics2017 <tracer@njuics.org>
2023-04-12 05:48:06 -0700

    >  compile 2013458 pa Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  05:48:06 up  1:50,  1 user,  load average: 0.54, 0.74, 0.80 4280956268fec6958751b52e7c3a641ffb50655d

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -46,7 +46,6 @@ make_EHelper(or) {
 make_EHelper(sar) {
   // TODO();
   // unnecessary to update CF and OF in NEMU
-  rtl_sext(&id_dest->val, &id_dest->val, id_dest->width);
   rtl_sar(&t2, &id_dest->val, &id_src->val);
   operand_write(id_dest, &t2);
   rtl_update_ZFSF(&t2, id_dest->width);
