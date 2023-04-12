1618573e92888995e42cccec211a0a074ffe2468
tracer-ics2017 <tracer@njuics.org>
2023-04-11 17:39:01 -0700

    >  compile 2013458 pa Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:39:01 up 23 min,  1 user,  load average: 0.36, 0.16, 0.11 6ef0442ec1e8c6b03a1734eca2e40d6e9df1541a

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -27,7 +27,6 @@ make_EHelper(call) {
   // the target address is calculated at the decode stage
   // TODO();
   rtl_push(&decoding.seq_eip);
-  cpu.eip = decoding.jmp_eip;
   decoding.is_jmp = 1;
 
   print_asm("call %x", decoding.jmp_eip);
