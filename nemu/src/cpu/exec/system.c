2bf46d315c265cdbed8c2019a5075213b2c6cc8e
tracer-ics2017 <tracer@njuics.org>
2023-05-05 06:42:07 -0700

    >  compile 2013458 pa Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  06:42:07 up  1:54,  1 user,  load average: 0.38, 0.38, 0.35 b2d48724ee0fa835f1afad3c7b26c2e47bac8c8d

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -32,7 +32,7 @@ make_EHelper(mov_cr2r) {
 
 extern void raise_intr(uint8_t NO, vaddr_t ret_addr);
 make_EHelper(int) {
-  // TODO();F_
+  // TODO();
   raise_intr(id_dest->val, decoding.seq_eip);
 
   print_asm("int %s", id_dest->str);
