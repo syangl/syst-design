734b7d9147e5fd0c4f390cf40c3b36e9da09da65
tracer-ics2017 <tracer@njuics.org>
2023-05-05 19:57:29 -0700

    >  compile 2013458 pa Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:57:29 up  2:01,  1 user,  load average: 2.02, 0.98, 0.66 67e95fa68e6dbe4575e9eee82030d8d54081c858

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,13 +1,46 @@
 #include "common.h"
 #include "syscall.h"
 
+
+
+
+static inline uintptr_t sys_none(_RegSet *r) {
+  SYSCALL_ARG1(r) = 1;
+  return (uintptr_t)1;
+} 
+
+static inline uintptr_t sys_exit(_RegSet *r) {
+  _halt(SYSCALL_ARG2(r));
+  return (uintptr_t)1;
+}
+
+// static inline uintptr_t sys_write(uintptr_t fd, uintptr_t buf, uintptr_t len) {
+// 	return fs_write(fd,(void *)buf,len);
+// }
+
+
+
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
+  a[1] = SYSCALL_ARG2(r);
+  a[2] = SYSCALL_ARG3(r);
+  a[3] = SYSCALL_ARG4(r);
 
   switch (a[0]) {
+    case SYS_none:
+      sys_none(r);
+      break;
+    case SYS_exit:
+      sys_exit(r);
+      break;
+    // case SYS_write:
+    //   SYSCALL_ARG1(r) = (int)sys_write(a[1],a[2],a[3]);
+    //   break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
   return NULL;
 }
+
