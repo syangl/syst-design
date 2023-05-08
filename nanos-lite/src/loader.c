e9e12892253dacc5bfaf7be3a993c3066f4f4ec7
tracer-ics2017 <tracer@njuics.org>
2023-05-07 18:22:13 -0700

    >  run 2013458 pa Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:22:13 up 45 min,  1 user,  load average: 0.89, 0.71, 0.69 9e8b0083a0f2fbe2ba539ec1eb5a26dd6b4d6e19

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -4,9 +4,18 @@
 
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
 extern size_t get_ramdisk_size();
+extern int fs_open(const char *pathname, int flags, int mode);
+extern ssize_t fs_read(int fd, void *buf, size_t len);
+extern ssize_t fs_write(int fd, const void *buf, size_t len);
+extern off_t fs_lseek(int fd, size_t offset, int whence);
+extern int fs_close(int fd);
+extern size_t fs_filesz(int fd);
 
 uintptr_t loader(_Protect *as, const char *filename) {
   // TODO();
-  ramdisk_read(DEFAULT_ENTRY, 0, get_ramdisk_size());
+  // ramdisk_read(DEFAULT_ENTRY, 0, get_ramdisk_size());
+  int fd = fs_open(filename, 0, 0);
+  fs_read(fd, (void *)DEFAULT_ENTRY, fs_filesz(fd));
+  fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
 }
