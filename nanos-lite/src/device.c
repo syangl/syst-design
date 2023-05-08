43e1ef4f5bb66641a080d508d29e3303f4021ea0
tracer-ics2017 <tracer@njuics.org>
2023-05-08 05:13:12 -0700

    >  run 2013458 pa Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  05:13:12 up  4:34,  1 user,  load average: 0.59, 0.52, 0.66 12728d3824f21a3bdd762456827ccf3d9d02cae7

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -42,12 +42,13 @@ void fb_write(const void *buf, off_t offset, size_t len) {
   offset /= 4;
   col = offset % _screen_width();
   row = offset / _screen_width();
+  Log("init_fs screen width: %d, screen height: %d", _screen_width(), _screen_height());
   _draw_rect((uint32_t *)buf, col, row, len / 4, 1);
 }
 
 void init_device() {
   _ioe_init();
-Log("init_fs screen width: %d, screen height: %d", _screen_width(), _screen_height());
+  // Log("init_fs screen width: %d, screen height: %d", _screen_width(), _screen_height());
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
   sprintf(dispinfo, "Width:%d\tHeight:%d\n", _screen_width(), _screen_height());
