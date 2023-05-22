ce25c8282c3387f589cf9b1ff0cec18aac8abb0d
tracer-ics2017 <tracer@njuics.org>
2023-05-22 02:32:52 -0700

    >  run 2013458 pa Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  02:32:52 up  2:51,  1 user,  load average: 1.16, 0.81, 0.79 7fba23721314e6c61f05b020138e5bef6004eb08

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -17,8 +17,9 @@ void load_prog(const char *filename) {
   // TODO: remove the following three lines after you have implemented _umake()
   _switch(&pcb[i].as);
   current = &pcb[i];
+  Log("before\n");
   ((void (*)(void))entry)();
-  Log("point\n");
+  Log("after\n");
   _Area stack;
   stack.start = pcb[i].stack;
   stack.end = stack.start + sizeof(pcb[i].stack);
