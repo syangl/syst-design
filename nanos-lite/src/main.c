09f225ef40fdbf26360794e2b083fa71093e357d
tracer-ics2017 <tracer@njuics.org>
2023-05-22 07:38:32 -0700

    >  run 2013458 pa Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  07:38:32 up  1:18,  1 user,  load average: 0.95, 0.52, 0.48 f78b1588fa3f02ece172fd4094c105836a1b88b9

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -36,8 +36,8 @@ int main() {
   // uint32_t entry = loader(NULL, "/bin/events");
   // uint32_t entry = loader(NULL, "/bin/pal");
   // ((void (*)(void))entry)();
-  load_prog("/bin/dummy");
-  // load_prog("/bin/pal");
+  // load_prog("/bin/dummy");
+  load_prog("/bin/pal");
   // load_prog("/bin/hello");
   _trap();
   
