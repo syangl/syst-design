788dcaa54dbd1e83ee56aa77b906c08a41243aa3
tracer-ics2017 <tracer@njuics.org>
2023-05-30 19:35:41 -0700

    >  compile 2013458 pa Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:35:41 up  1:43,  1 user,  load average: 1.12, 0.92, 0.88 5e2b1d0b92ba9e34d9cde7e93a4b6108c854701e

--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -44,36 +44,36 @@ FLOAT f2F(float a) {
    */
 
   assert(0);
-  union turn_float{
-    struct {
-      uint32_t mantissa : 23;
-      uint32_t exp : 8;
-      uint32_t sign : 1;
-    };
-    uint32_t val;
-  };
+  // union turn_float{
+  //   struct {
+  //     uint32_t mantissa : 23;
+  //     uint32_t exp : 8;
+  //     uint32_t sign : 1;
+  //   };
+  //   uint32_t val;
+  // };
 
-  union turn_float f;
-  f.val = *((uint32_t*)(void*)&a);
+  // union turn_float f;
+  // f.val = *((uint32_t*)(void*)&a);
 
-  FLOAT ret = 0;
+  // FLOAT ret = 0;
 
-  int exp = f.exp - 127;
-  if (exp < 0){
-    return 0;
-  }else{
-    if (exp - 7 >= 0){
-      ret = (f.mantissa | (1 << 23)) << (exp - 7);
-    }else{
-      ret = (f.mantissa | (1 << 23)) >> (7 - exp);
-    }
-  }
+  // int exp = f.exp - 127;
+  // if (exp < 0){
+  //   return 0;
+  // }else{
+  //   if (exp - 7 >= 0){
+  //     ret = (f.mantissa | (1 << 23)) << (exp - 7);
+  //   }else{
+  //     ret = (f.mantissa | (1 << 23)) >> (7 - exp);
+  //   }
+  // }
 
-  if (f.sign != 0){
-    ret = -ret;
-  }
-  // assert(a != 0 && ret == 0);
-  return ret;
+  // if (f.sign != 0){
+  //   ret = -ret;
+  // }
+  // // assert(a != 0 && ret == 0);
+  // return ret;
 }
 
 FLOAT Fabs(FLOAT a) {
